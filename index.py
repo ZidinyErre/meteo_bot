a95a0acb86824568d1c3c2859d7f307c29c22f13
Zidiny Erre <zidiny.erre.fr@gmail.com>
2025-09-17 11:22:49 +0200

    Try to return the weather data

--- index.py
+++ index.py
@@ -6,7 +6,10 @@ driver = webdriver.Chrome()
 driver.get("https://meteofrance.com/previsions-meteo-france/montpellier/34000")
 
 element = driver.find_element(By.CSS_SELECTOR,".weather_temp")
-scd_element = element.find_element(By.TAG_NAME, "p")
+scd_element = element.find_element(By.TAG_NAME, "p").text
+
+#  Add the .text after scd_element . Same err = GPU state invalid after WaitForGetOffsetInRange
+# Reading of the doc in option part to find the solution 
 
 print(scd_element)
 
