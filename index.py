558125e43428ab7a2031302e8d4a3535bde60b77
Zidiny Erre <zidiny.erre.fr@gmail.com>
2025-09-17 23:15:15 +0200

    First weather data ok !

--- index.py
+++ index.py
@@ -1,15 +1,23 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 
 # With Selenium i can use a webdriver to read a webpage in different navigator
-driver = webdriver.Chrome()
+
+# Here i can make option and use the driver in different way. I disable the GPU because the script 
+# do not support the use of the graphic cards
+options = Options()
+# the option headless mean we not gonna see our bot act 
+options.add_argument("--headless")
+options.add_argument("--disable-gpu")
+driver = webdriver.Chrome(options=options)
+
 driver.get("https://meteofrance.com/previsions-meteo-france/montpellier/34000")
 
 element = driver.find_element(By.CSS_SELECTOR,".weather_temp")
 scd_element = element.find_element(By.TAG_NAME, "p").text
 
-#  Add the .text after scd_element . Same err = GPU state invalid after WaitForGetOffsetInRange
-# Reading of the doc in option part to find the solution 
+
 
 print(scd_element)
 
