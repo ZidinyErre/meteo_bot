d630a499a2efb4359da4ada1f35464de05bf67fb
Zidiny Erre <zidiny.erre.fr@gmail.com>
2025-09-23 16:49:25 +0200

    Time data ok !

--- index.py
+++ index.py
@@ -15,10 +15,20 @@ driver = webdriver.Chrome(options=options)
 driver.get("https://meteofrance.com/previsions-meteo-france/montpellier/34000")
 
 element = driver.find_element(By.CSS_SELECTOR,".weather_temp")
-scd_element = element.find_element(By.TAG_NAME, "p").text
+temperature = element.find_element(By.TAG_NAME, "p").text
+
+# I need to take the time in .period
+# To Have the "soirée" part i can make a if time_element variable by tag_name p = "Soirée" then tell me (temp , weather)
+time_element = driver.find_element(By.CSS_SELECTOR, ".period").text
+
+print(temperature)
+print(time_element)
+
+# I need to take the title or the alt of the img in .weather_temp
+# I need to understand how wind information wroks and take the information
 
 
+# I need to find how i can send data to my phone by is number 
 
-print(scd_element)
 
 driver.quit()
